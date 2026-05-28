2283c2b2d2a39a7c25bae0fcc319d513dffd8082
zero5yt <roderickalmaras05@gmail.com>
2026-05-28 12:50:18 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Install dependencies
+# 1. Install python packages
 pip install -r requirements.txt
 
-# 2. Ituro ang folder kung saan dapat i-save ang browser
+# 2. Ituro ang folder (Persistent Cache)
 export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.cache/ms-playwright
 
-# 3. Install Chromium (Pati dependencies para sigurado)
-python -m playwright install --with-deps chromium
+# 3. I-install ang Chromium binary LANG (Wag na yung dependencies)
+python -m playwright install chromium
